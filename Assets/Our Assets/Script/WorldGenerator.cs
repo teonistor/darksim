1d2b5229ceea0b47365a0d7aee69ebe64094899c
Teo G Nistor <teo.g.nistor@gmail.com>
2018-05-03 19:46:18 +0100

    Pull ai scene

---
+++ Assets/Our Assets/Script/WorldGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGenerator : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject wall;
+	// Use this for initialization
+	void Start () {
+        Instantiate(wall, new Vector3(-3,0.5f,-3), Quaternion.identity);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
