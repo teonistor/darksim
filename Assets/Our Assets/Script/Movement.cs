c97acd1e32249ccdfa497680a5ca40153e3cd6dc
Teo G Nistor <teo.g.nistor@gmail.com>
2018-05-03 19:01:37 +0100

    Lumina move

---
+++ Assets/Our Assets/Script/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    [SerializeField] private float speed = 0.1f;
+    
+	void Start () {
+		
+	}
+
+	void Update () {
+        Vector3 v = new Vector3();
+        v.x = Input.GetAxis("Horizontal");
+        v.z = Input.GetAxis("Vertical");
+        v = Vector3.ClampMagnitude(v, 1f) * speed;
+
+        transform.position += v;
+    }
+}
