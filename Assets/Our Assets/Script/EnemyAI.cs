c57eac66b46fcae96b33f28006a7bcd08e179d0f
Teo G Nistor <teo.g.nistor@gmail.com>
2018-05-04 14:14:28 +0100

    Pull Stub enemy generator

---
+++ Assets/Our Assets/Script/EnemyAI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyAI : MonoBehaviour {
+
+    private WorldGenerator worldGen;
+
+    private int[,] world;
+
+    private Vector2 location;
+    void Start () {
+        location = new Vector2();
+	}
+
+    void SetUp(Vector2 location, WorldGenerator worldGen) {
+        this.location = location;
+        this.worldGen = worldGen;
+        world = worldGen.getWorld();
+    }
+
+    void Astar(Vector2 target) {
+        
+    }
+    public void SetLocation(Vector2 newLoc) {
+        location = newLoc;
+    }
+}
--- Assets/Our Assets/Script/WorldGenerator.cs
+++ Assets/Our Assets/Script/WorldGenerator.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
-
+using System.Collections.Generic;
 public class WorldGenerator : MonoBehaviour {
 
+    //prefabs for wall and enemy
     [SerializeField]
     private GameObject wall;
-    // Use this for initialization
+    [SerializeField]
+    private GameObject enemy;
+
+    //gameobject of the player
+    [SerializeField]
+    private GameObject player;
 
     // matrix used for world generation - origin is at top-left.
     private int[,] world;
@@ -18,8 +24,19 @@ public class WorldGenerator : MonoBehaviour {
     //variable used for player's position: x is the position on 'width' and y is the position on 'height'
     private Vector2 playerPosition;
 
+    //distance between enemy and player 
     [SerializeField]
-    private GameObject player;
+    private int dist = 30;
+    //number of enemies 
+    [SerializeField]
+    private int numberOfEnemies = 1;
+
+    //Codes used in generating the map
+    public const int wallCode = 1;
+    public const int objectCode = 2;
+    public const int enemyCode = 3;
+
+
     void Start() {
         world = new int[height, width];
 
@@ -35,6 +52,9 @@ public class WorldGenerator : MonoBehaviour {
         //executing function to generate the map
         generateRandomWorld();
 
+        //executing function to generate enemies;
+        generateEnemies(numberOfEnemies,dist);
+
         //executing function to draw the map
         drawMap();
     }
@@ -44,10 +64,10 @@ public class WorldGenerator : MonoBehaviour {
     /// </summary>
     void pad() {
         for (int i = 0; i < height; ++i) {
-            world[i, width - 1] = world[i, 0] = 1;
+            world[i, width - 1] = world[i, 0] = wallCode;
         }
         for (int j = 0; j < width; ++j) {
-            world[0, j] = world[height - 1, j] = 1;
+            world[0, j] = world[height - 1, j] = wallCode;
         }
     }
 
@@ -67,6 +87,57 @@ public class WorldGenerator : MonoBehaviour {
     }
 
     /// <summary>
+    /// bfs function that generate the enemies.
+    /// </summary>
+    /// <param name="numberOfEnemies"> integer indicating the number of enemies</param>
+    /// <param name="distance"> integer indicating the minimum distance between the enemies and the player</param>
+    void generateEnemies(int numberOfEnemies, int distance) {
+
+        int[,] mapChecker = new int[height, width]; 
+
+        //queue elements are vector3, with x,y the coordinates and z the distance between the given point and the player
+        //queue used for bfs
+
+        Queue<Vector3> qu = new Queue<Vector3>();
+        //adding the player position to the queue
+        qu.Enqueue(new Vector3(playerPosition.x, playerPosition.y, 1));
+
+        //list containing the possible enemy locations
+        List<Vector2> possibleEnemyLoc = new List<Vector2>();
+
+        //bfs
+        while(qu.Count != 0) {
+            Vector3 currentPos = qu.Dequeue();
+            
+            //checking if current position was visited
+            if(mapChecker[(int)currentPos.x, (int)currentPos.y] != 0)
+                continue;
+            //checking if current position is empty
+            if (world[(int)currentPos.x, (int)currentPos.y] != 0)
+                continue;
+
+            mapChecker[(int)currentPos.x, (int)currentPos.y] = (int)currentPos.z;
+
+            qu.Enqueue(currentPos + new Vector3(1, 0, 1));
+            qu.Enqueue(currentPos + new Vector3(0, 1, 1));
+            qu.Enqueue(currentPos + new Vector3(-1, 0, 1));
+            qu.Enqueue(currentPos + new Vector3(0, -1, 1));
+
+            if(mapChecker[(int)currentPos.x, (int)currentPos.y] > distance) {
+                possibleEnemyLoc.Add(new Vector2(currentPos.x, currentPos.y));
+            }
+        }
+
+        System.Random rnd = new System.Random();
+        while(numberOfEnemies > 0) {
+            int nextEnemyIndex = rnd.Next(0, possibleEnemyLoc.Count);
+            Vector2 nextEnemy = possibleEnemyLoc[nextEnemyIndex];
+            possibleEnemyLoc.RemoveAt(nextEnemyIndex);
+            world[(int)nextEnemy.x, (int)nextEnemy.y] = enemyCode;
+            numberOfEnemies--;
+        }
+    }
+    /// <summary>
     /// Function that draws the world.
     /// </summary>
     void drawMap() {
@@ -79,12 +150,20 @@ public class WorldGenerator : MonoBehaviour {
                  * 0 -> empty space
                  * 1 -> wall
                  * 2 -> special object
+                 * 3 -> enemy
                  */
 
-                if (world[i, j] == 1) {
+                if (world[i, j] == wallCode) {
                     Instantiate(wall, new Vector3(i, wall.transform.localScale.y / 2, j), Quaternion.identity);
+                }else if (world[i, j] == enemyCode) {
+                    GameObject newEnemy = Instantiate(enemy, new Vector3(i, enemy.transform.localScale.y / 2, j), Quaternion.identity);
+                    newEnemy.GetComponent<EnemyAI>().SetLocation(new Vector2(i,j));
                 }
             }
         }
+    }
+
+    public int[,] getWorld() {
+        return world;
     }
 }
