c516ea307a10bbd03e30171b79d9d80d02d950e6
Teo G Nistor <teo.g.nistor@gmail.com>
2018-05-04 12:37:46 +0100

    Auto tile texture (taken from game 1)

--- Assets/Our Assets/Script/Collect.cs
+++ Assets/Our Assets/Script/Collect.cs
@@ -18,9 +18,10 @@ public class Collect : MonoBehaviour {
         }
 	}
 
+    // For demo unity logo
     private Vector3 tp = new Vector3(-8f, 4f, -8f),
-                    tr = new Vector3(900f, -900f, 900f),
-                    ts = new Vector3(1f, 1f, -1f);
+                    tr = new Vector3(22.8f, 96.8f, -26.6f),
+                    ts = new Vector3(1f, 1.03f, -1f);
 
     private IEnumerator collectAnimation() {
         float frame = 1f / 30;
@@ -31,7 +32,7 @@ public class Collect : MonoBehaviour {
                 sr = transform.localEulerAngles,
                 ss = transform.localScale;
 
-        for (float t=0f;t<=1f ; t += frame) {
+        for (float t=0f;t<1.1f ; t += frame) {
             transform.localPosition = Vector3.Lerp(sp, tp, t);
             transform.localEulerAngles = Vector3.Lerp(sr, tr, t);
             transform.localScale = Vector3.Lerp(ss, ts, t);
---
+++ Assets/Our Assets/Script/TileTexture.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class TileTexture : MonoBehaviour {
+	void Start () {
+        GetComponent<Renderer>().material.mainTextureScale = transform.localScale;
+        // GetComponent<Renderer>().material.mainTextureScale = new Vector2(transform.localScale.x, transform.localScale.z);
+	}
+}
