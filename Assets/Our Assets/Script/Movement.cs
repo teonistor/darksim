dcaaaecfa23195b0c2c41c0ab813568628a49d37
Teo G Nistor <teo.g.nistor@gmail.com>
2018-05-04 14:10:58 +0100

    Backup movement obstacle det before pull

--- Assets/Our Assets/Script/Movement.cs
+++ Assets/Our Assets/Script/Movement.cs
@@ -4,18 +4,56 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
+    public enum Side { Up, Right, Down, Left };
+
+
     [SerializeField] private float speed = 0.1f;
+
+    public int[] blockedMove = new int[] { 0,0,0,0 };
     
-	void Start () {
-		
-	}
+
+    private bool blockedUp {
+        get {
+            return blockedMove[(int)Side.Up] > 0;
+        }
+    }
+
+    private bool blockedRight {
+        get {
+            return blockedMove[(int)Side.Right] > 0;
+        }
+    }
+
+    private bool blockedDown {
+        get {
+            return blockedMove[(int)Side.Down] > 0;
+        }
+    }
+
+    private bool blockedLeft {
+        get {
+            return blockedMove[(int)Side.Left] > 0;
+        }
+    }
+
 
 	void Update () {
         Vector3 v = new Vector3();
+
+        // Get movement input from axes
         v.x = Input.GetAxis("Horizontal");
         v.z = Input.GetAxis("Vertical");
+
+        // Block movement when hitting stuff
+        if (blockedUp && v.z > 0f) v.z = 0f;
+        if (blockedRight && v.x > 0f) v.x = 0f;
+        if (blockedDown && v.z < 0f) v.z = 0f;
+        if (blockedLeft && v.x < 0f) v.x = 0f;
+
+        // Normalise (no diagonal speedup)
         v = Vector3.ClampMagnitude(v, 1f) * speed;
 
+        // Move
         transform.position += v;
     }
 }
---
+++ Assets/Our Assets/Script/ObstacleDetector.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class ObstacleDetector : MonoBehaviour {
+    [SerializeField] private Movement.Side side;
+
+    private Movement parent;
+
+    void Start () {
+        parent = GetComponentInParent<Movement>();
+    }
+
+    void OnTriggerEnter (Collider other) {
+        parent.blockedMove[(int)side]++;
+    }
+
+    void OnTriggerExit(Collider other) {
+        parent.blockedMove[(int)side]--;
+    }
+}
