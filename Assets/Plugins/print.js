d8e6b38284a3374e443b853b3131f9afc2cca259
Teo G Nistor <teo.g.nistor@gmail.com>
2018-05-13 15:24:21 +0100

    Attempt to create external JS print capacity

---
+++ Assets/Plugins/print.js
@@ -0,0 +1,12 @@
+/*mergeInto(LibraryManager.library, {
+
+  Hello: function () {
+    window.alert("Hello, world!");
+  },
+
+  PrintJS: function (str) {
+    //window.alert(Pointer_stringify(str));
+	console.log(Pointer_stringify(str));
+  }
+});
+*/
